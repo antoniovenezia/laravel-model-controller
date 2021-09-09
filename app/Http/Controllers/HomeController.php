2d4886b7503f242fd50a22a75242d4b75208717a
antoniovenezia <antoniovenezia00@gmail.com>
2021-09-09 17:30:58 +0200

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Movie;
+
+
+class HomeController extends Controller
+{
+    public function index(){
+        // return view('welcome')
+        $allMovies = Movie::all();
+        // dump($allMovies);
+
+        return view('home', compact($allMovies));
+    }
+}
